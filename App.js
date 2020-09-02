190a0eef57cf5a8cd62fb4314456056d61698063
Ihar Tsykala <Sudja09@bk.ru>
2020-09-02 20:44:23 +0300

    feat: add api

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+    const [state, setState] = useState('12')
+    
+    const currentPosition = async () => {
+        try {
+            const response = await fetch('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDDuAn5EN9v9O3dVjMPXP-vnV4MyowsY4w')
+            console.log(response)
+            setState(response)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress ={()=>currentPosition()}>{`Hello31273: `}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +31,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+// ${state.toString()}
